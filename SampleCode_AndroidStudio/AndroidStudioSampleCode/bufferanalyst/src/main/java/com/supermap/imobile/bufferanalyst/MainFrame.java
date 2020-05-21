1440614e17bf9ab3c22c18bca8159a3b5bbd3a4d
Wangli-in <wangli_hbhs@163.com>
2020-05-21 20:15:14 +0800

    修改内容：缓冲区分析示范工程修改

--- SampleCode_AndroidStudio/AndroidStudioSampleCode/bufferanalyst/src/main/java/com/supermap/imobile/bufferanalyst/MainFrame.java
+++ SampleCode_AndroidStudio/AndroidStudioSampleCode/bufferanalyst/src/main/java/com/supermap/imobile/bufferanalyst/MainFrame.java
@@ -96,21 +96,10 @@ public class MainFrame extends Activity implements OnTouchListener{
 	private Button m_btnQuery;
 	private EditText m_edtDistance;
 	String sdcard = android.os.Environment.getExternalStorageDirectory().getAbsolutePath().toString();
-	/**
-	 * 需要申请的权限数组
-	 */
-	protected String[] needPermissions = {
-			Manifest.permission.WRITE_EXTERNAL_STORAGE,
-			Manifest.permission.ACCESS_FINE_LOCATION,
-			Manifest.permission.READ_PHONE_STATE,
-			Manifest.permission.ACCESS_WIFI_STATE,
-			Manifest.permission.ACCESS_NETWORK_STATE,
-			Manifest.permission.CHANGE_WIFI_STATE,
-	};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		requestPermissions();
 
         // 初始化环境,设置许可路径
         Environment.setLicensePath(sdcard+"/SuperMap/license/");
@@ -127,45 +116,7 @@ public class MainFrame extends Activity implements OnTouchListener{
 
         initView();
     }
-	/**
-	 * 检测权限
-	 * return true:已经获取权限
-	 * return false: 未获取权限，主动请求权限
-	 */
 
-	public boolean checkPermissions(String[] permissions) {
-		return EasyPermissions.hasPermissions(this, permissions);
-	}
-
-	/**
-	 * 申请动态权限
-	 */
-	private void requestPermissions() {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-			return;
-		}
-		if (!checkPermissions(needPermissions)) {
-			EasyPermissions.requestPermissions(
-					this,
-					"为了应用的正常使用，请允许以下权限。",
-					0,
-					Manifest.permission.WRITE_EXTERNAL_STORAGE,
-					Manifest.permission.ACCESS_FINE_LOCATION,
-					Manifest.permission.READ_PHONE_STATE,
-					Manifest.permission.ACCESS_WIFI_STATE,
-					Manifest.permission.ACCESS_NETWORK_STATE,
-					Manifest.permission.CHANGE_WIFI_STATE);
-			//没有授权，编写申请权限代码
-		} else {
-			//已经授权，执行操作代码
-		}
-	}
-	@Override
-	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-		// Forward results to EasyPermissions
-		EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-	}
  // 打开地图
     private boolean openMap(){
     	
